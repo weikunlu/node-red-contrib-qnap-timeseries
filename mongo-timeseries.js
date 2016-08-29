5feb66d86ceb649e51e87b52c3fe89f537d4f300
weikun lu <weikun.lu@gmail.com>
2016-08-29 12:24:39 +0800

    implement mongo aggre node function.

--- mongo-timeseries.js
+++ mongo-timeseries.js
@@ -162,4 +162,153 @@ module.exports = function(RED) {
 	RED.nodes.registerType("mongodb out", MongoOutNode);
 // --------------- end of MongoOutNode
 
+	function getLimitCount(resolution, interval, range){
+		var resolutionSize = 0;
+		switch(resolution){
+			case 'rm': // minute
+			resolutionSize = 60 * 24;
+			break;
+			case 'rh': // hour
+			if(range=='d')
+				resolutionSize = 24;
+			else if(range=='w')
+				resolutionSize = 24*7;
+			break;
+			case 'rd': // day
+			if(range=='w')
+				resolutionSize = 7;
+			else if(range=='m')
+				resolutionSize = 30;
+			break;
+		}
+		return (resolutionSize/interval);
+	}
+
+	function getResolutionConfig(resolution, interval){
+		var config = {};
+		switch(resolution){
+			case 'rm': // minute
+				config["Y"] = {$year:"$timestamp"};
+				config["M"] = {$month:"$timestamp"};
+				config["D"] = {$dayOfMonth:"$timestamp"};
+				config["H"] = {$hour:"$timestamp"};
+
+				if(interval==1){
+					config["m"] = {$minute:"$timestamp"};
+				}else{
+					config["m"] = {$subtract:[{$minute:"$timestamp"}, {$mod:[{$minute:"$timestamp"}, interval]}]};
+				}
+			break;
+			case 'rh': // hour
+				config["Y"] = {$year:"$timestamp"};
+				config["M"] = {$month:"$timestamp"};
+				config["D"] = {$dayOfMonth:"$timestamp"};
+
+				if(interval==1){
+					config["H"] = {$hour:"$timestamp"};
+				}else{
+					config["H"] = {$subtract:[{$hour:"$timestamp"}, {$mod:[{$hour:"$timestamp"}, interval]}]};
+				}
+
+			break;
+			case 'rd': // day
+				config["Y"] = {$year:"$timestamp"};
+				config["M"] = {$month:"$timestamp"};
+				config["D"] = {$dayOfMonth:"$timestamp"};
+			break;
+		}
+		return config;
+	}
+
+	function MongoAggreNode(n) {
+		RED.nodes.createNode(this, n);
+		this.deviceid = n.deviceid;
+		this.sensorid = n.sensorid;
+		this.resolution = n.resolution;
+		this.interval = n.interval;
+		this.range = n.range;
+		this.mongodb = n.mongodb;
+		this.mongoConfig = RED.nodes.getNode(this.mongodb);
+
+		if(!this.mongoConfig){
+			this.error(RED._("mongodb.errors.nodbconfig"));
+			return;
+		}
+
+		var node = this;
+		var url = this.mongoConfig.url;
+		MongoClient.connect(url, function(err, db) {
+
+			if (err) {
+				node.error(err);
+				return;
+			}
+
+			node.clientDb = db;
+			node.on("input",function(msg) {
+
+				var deviceId = node.deviceid;
+				var sensorId = node.sensorid;
+
+				var int_interval = parseInt(node.interval);
+				var coll = db.collection(node.resolution+deviceId);
+				var limit = getLimitCount(node.resolution, int_interval, node.range);
+
+				var matchObj = {};
+				matchObj["sensorId"] = sensorId;
+				if(msg.payload.start_date && msg.payload.end_date){
+					matchObj["timestamp"] = {$gte: new Date(), $lte: new Date()};
+				}else{
+					if(msg.payload.start_date){
+						// var startTime = new Date();
+						// var endTime = new Date();
+						// endTime.setHours(endTime.getHours() - 2);
+						// "timestamp":{$gte: ed, $lte: new Date()}
+						matchObj["timestamp"] = {$gte: new Date()};
+					}
+
+					if(msg.payload.end_date){
+						matchObj["timestamp"] = {$lte: new Date()};
+					}
+				}
+
+				var aggrs = [];
+				aggrs.push({$match: matchObj });
+				aggrs.push({$group: 
+							{ "_id": getResolutionConfig(node.resolution, int_interval), 
+							"mysum": {$sum : "$sum"}, 
+							"mycount": {$sum : "$count"}, 
+							"vmax": {$max:"$vmax"}, 
+							"vmin": {$min:"$vmin"} }
+						});
+
+				aggrs.push({$project: {"_id":1, "vavg": {$divide: ["$mysum","$mycount"]}, "vmax":1, "vmin":1}});
+				aggrs.push({$sort: {"_id":-1} });
+				aggrs.push({$limit: limit });
+
+				console.log('[debug]'+JSON.stringify(aggrs));
+
+				coll.aggregate(aggrs)
+					.toArray(function(err, result) {
+						if(err){
+							node.error(err);
+							return;
+						}
+						var qresult = {payload: {query:matchObj, datas:result}};
+						node.send(qresult);
+					});
+			});
+
+		});
+
+		this.on("close", function() {
+			if (this.clientDb) {
+				this.clientDb.close();
+			}
+		});
+
+	}
+
+	RED.nodes.registerType("mongodb aggre", MongoAggreNode);
+
 }
